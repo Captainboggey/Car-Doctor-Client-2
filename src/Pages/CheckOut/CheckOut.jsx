d883d91071a6b7b21eaef7822e04aa13423bc385
captainboogey <anik35-700@diu.edu.bd>
2025-02-18 02:49:10 +0600

    git cpmmit-m"modified"

--- src/Pages/CheckOut/CheckOut.jsx
+++ src/Pages/CheckOut/CheckOut.jsx
@@ -3,13 +3,42 @@ import { useLoaderData } from 'react-router-dom';
 
 const CheckOut = () => {
     const itemInfo =useLoaderData() 
-    console.log(itemInfo)
+    const {title,img,price}=itemInfo
+    const handleOrder = e =>{
+        e.preventDefault()
+        const form = e.target;
+        const first = form.first.value;
+        const last =form.last.value;
+        const phone = form.phone.value;
+        const email = form.email.value;
+        const message = form.message.value;
+        const orderInfo = {
+            User: first,last,phone,email,message,
+            title: title,
+            img: img,
+            price: price
+            
+        }
+        fetch("http://localhost:5000/bookings",{
+            method:"POST",
+            headers:{
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(orderInfo)
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+        })
+       
+
+    }
     return (
         <div>
        <div className='w-48 mx-auto'>
        <button className='text-center btn p-2 tab '>Home/Checkout</button>
        </div>
-       <form>
+       <form onSubmit={handleOrder}>
         <div className='grid  lg:grid-cols-2 gap-10 bg-base-200 lg:w-full p-10 lg:p-20'>
         <div className='form-control'>
            
@@ -21,14 +50,14 @@ const CheckOut = () => {
          </div>
         <div className='form-control'>
           
-           <input type="text" placeholder='Your Phone' name='name' className='input input-bordered'/>
+           <input type="text" placeholder='Your Phone' name='phone' className='input input-bordered'/>
          </div>
         <div className='form-control'>
           
            <input type="email" required placeholder='Your Email' name='email' className='input input-bordered'/>
          </div>
         <div className='form-control md:col-span-2 '>
-           <label><span className='label-text'>Name:</span> </label>
+           
            <input type="text" placeholder='Your Message' name='message' className='input input-bordered lg:h-44'/>
          </div>
         <div className='form-control md:col-span-2'>
